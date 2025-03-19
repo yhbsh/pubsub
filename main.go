c53655e9542b02663f84997e3b05b6c852cfe250
yhbsh <houssembousmaha.ksu@gmail.com>
2025-03-19 07:28:19 +0300

    gofmt

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 )
 
 func main() {
-    for i := 9000; i < 9020; i++ {
-        server := server.New(i)
-        go server.Run()
-    }
+	for i := 9000; i < 9020; i++ {
+		server := server.New(i)
+		go server.Run()
+	}
 
 	signalchannel := make(chan os.Signal, 1)
 	signal.Notify(signalchannel, syscall.SIGINT, syscall.SIGTERM)
-    <-signalchannel
-    log.Print("Exiting ...")
+	<-signalchannel
+	log.Print("Exiting ...")
 }
